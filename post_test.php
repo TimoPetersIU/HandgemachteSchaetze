a349b3bfa4bd92d4203fe17924a06e38bded2a5b
Timo Peters <timopeters95@gmail.com>
2023-11-12 15:14:18 +0100

    php Funktionalitaet und spielerei

---
+++ post_test.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<body>
+<?php
+$servername = "localhost";
+$user = "root";
+$password = "";
+$db = "HandgemachteSchaetze";
+
+$connection = new mysqli($servername, $user, $password, $db);
+
+$name = $_POST["name"];
+$email = $_POST["email"];
+$date = $_POST["birth_date"];
+
+$sql_insert = "INSERT INTO `artists`(`name`, `email`, `date_of_birth`) VALUES ('{$name}','{$email}', '{$date}')";
+
+if ($connection->query($sql_insert) === TRUE) {
+    echo "INSERT SUCCESSFUL";
+} else {
+    echo "INSERT FAILED" . $connection->error;
+}
+
+$connection->close();
+
+?>
+</body>
