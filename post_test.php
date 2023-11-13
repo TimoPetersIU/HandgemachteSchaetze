e5430854b1fea57241928e5b79601526941860b9
Timo Peters <timopeters95@gmail.com>
2023-11-13 09:37:35 +0100

    db connection ausgelagert

--- post_test.php
+++ post_test.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <body>
 <?php
-$servername = "localhost";
-$user = "root";
-$password = "";
-$db = "HandgemachteSchaetze";
-
-$connection = new mysqli($servername, $user, $password, $db);
+require("connection.php");
+//$servername = "localhost";
+//$user = "root";
+//$password = "";
+//$db = "HandgemachteSchaetze";
+//
+//$connection = new mysqli($servername, $user, $password, $db);
 
 $name = $_POST["name"];
 $email = $_POST["email"];
---
+++ shopping_cart/shopping_cart.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <title>Warenkorb</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+<?php
+session_start();
+require("../connection.php");
+
+?>
+</body>
