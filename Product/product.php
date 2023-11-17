ee5272a9d5479cb8af9edae25d129375e9af5327
DomIUik <dominik.skalka@iu-study.org>
2023-11-18 00:43:51 +0100

    alte codefetzel entfernt

--- Product/product.php
+++ Product/product.php
@@ -16,7 +16,6 @@ $_GET["artist_name"];
 require("../connection.php");
 
 // SQL-Abfrage für Produkte
-//datenbank noch wechseln
 if($_GET["id"] > 0){
 $sql = "SELECT * FROM products join artists on  products.artist_name = artists.name where products.id = '".$connection->real_escape_string($_GET['id']). "'" ;
 }
@@ -25,58 +24,14 @@ else{
 }
 $result = $connection->query($sql);
 
-// Überprüfen, ob Ergebnisse vorhanden sind
-/* if ($result->num_rows > 0) {
-    // Daten aus der Abfrage abrufen
-    $row = $result->fetch_assoc();
 
-    // HTML-Tabelle mit dynamischen Daten füllen
-    echo '<table cellspacing="0" cellpadding="0">';
-    echo '<tr>';
 
-    echo '<th colspan="3">' . $row['pname'] . '</th>';
-    echo '</tr>';
-    echo '<tr class="cell">';
-    echo '<td>' . $row['artist_name'] . '</a></td>';
-    echo '<td>' . $row['price'] . '€</td>';
-
-    echo '</tr>';
-    echo '<tr>';
-    echo '<td>' . $row['email'] . '</td>';
-    echo '<td>' . $row['category'] . '</td>';
-    echo '</tr>';
-
-    echo '<tr>';
-    echo '<td colspan="3"><img src="../Bildes/' . $row['img_path'] . '" alt="bild von ' . $row['pname'] . '"></td>';
-    echo '</tr>';
-    echo '<tr>';
-    echo '<td colspan="3">' . $row['description'] . '</td>';
-    echo '</tr>';
-    echo '<tr>';
-    if ($row['availability'] == 1) {
-        echo '<td colspan="3">Lagerbestand '. $row['availability'].'</td>'; }
-    else { 
-        echo'<td colspan="3">Nicht auf Lager </td>';}  
-    echo '</tr>';
-    echo '<tr>';
-    echo '<th colspan="1"><a href="../tabllenexperiment.php">Zurück</a></th>';
-    echo '<th colspan="2">Warenkorb</th>';
-    echo '</tr>';
-    echo '</table>';
-} else {
-    echo "Keine Ergebnisse gefunden";
-}
-*/
-// <p><a href="Product/product.php?id=25">cooles zeug</a></p>
 if ($result) {
     // Daten der Db als Tabelle ausgeben und auf der Webseite anzeigen
     while ($row = $result->fetch_assoc()) {
         if(!file_exists("../Bildes/" . $row['img_path']) || empty($row['img_path'])){
         $row['img_path'] = 'noimage.png';   
         }
-//echo "<div class='klick'>";
-  //          echo "<a href='Product/product.php?id=$row[id]'> ";
-        //    echo" <button class='choice_btn' >";
         echo "<div class='klick'>";
         echo "<a href='?id=$row[id]'> ";
                     echo "<table class='choice_table' cellspacing=0 cellpadding=0>";
@@ -119,7 +74,6 @@ if ($result) {
                 echo "</table>";
                 echo "</a>";
                 echo "</div>";
-    // Verbindung schließen
 }
 }
 // Verbindung schließen
--- Tabllenexperiment.php
+++ Tabllenexperiment.php
@@ -12,30 +12,23 @@
                     <div class="table_div container">
                         <?php
                          require("connection.php");
-                            
-                            // Verbindung zur Datenbank herstellen
-                           
-                            
+                     
                             // Überprüfen, ob die Verbindung erfolgreich hergestellt wurde
                             if ($connection->connect_error) {
                                 die("Connection failed: " . $connection->connect_error);
                             }
-// max seite, mAIN hubsch, mehr bildes, kommentieren
                             $sql = "SELECT * FROM products where availability = 1";
 
                             // Die Abfrage ausführen und das Ergebnis speichern
                             $result = $connection->query($sql);
                             
-                            // Überprüfen, ob die Abfrage erfolgreich war
                             if ($result) {
-                                // Daten der Db als Tabelle ausgeben und auf der Webseite anzeigen
                                 while ($row = $result->fetch_assoc()) {
                                     if(!file_exists("Bildes/" . $row['img_path']) || empty($row['img_path'])){
                                     $row['img_path'] = 'noimage.png';
                                     }
                                         echo "<div class='klick'>";
                                         echo "<a href='Product/product.php?id=$row[id]'> ";
-                                    //    echo" <button class='choice_btn' >";
                                             echo "<table class='choice_table' cellspacing=0 cellpadding=0>";
                                             echo "<tr>";
                                             echo "<th class='td pname'>". $row["pname"]."</th>";
@@ -46,7 +39,7 @@
                                             echo "</tr>";
                                             echo "<tr>";
                                             echo '<td colspan="2"><div class = "product_image" style="background-image:url(Bildes/' . $row['img_path'] . ');"></div></td>';
-                                           // " alt="bild von ' . $row['pname'] . '">
+
                                             echo "</tr>";
                                             echo "<tr>";
                                         
@@ -54,7 +47,7 @@
                                             echo "</tr>";
                                            
                                             echo "</table>";
-                                  //      echo "</button>";
+
                                         echo "</a>";
                                         echo "</div>";
                                 }
