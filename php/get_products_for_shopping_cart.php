9c6e7f3b3e54addc9f8f4d16d4b587e18ef90058
Timo Peters <timopeters95@gmail.com>
2023-11-19 14:03:47 +0100

    Kommentare

--- php/get_products_for_shopping_cart.php
+++ php/get_products_for_shopping_cart.php
@@ -1,35 +1,49 @@
 <?php
+// Verbindung zur Datenbank herstellen
 require("connection.php");
 
+// Überprüfen, ob Produkt-IDs im POST-Request vorhanden und nicht leer sind
 if (isset($_POST['productIds']) && !empty($_POST['productIds'])) {
+    // Produkt-IDs aus dem POST-Request extrahieren
     $ids = $_POST['productIds'];
 
+    // Produkt-IDs zu einer Zeichenkette konvertieren
     $productIdList = implode(',', $ids);
 
+    // SQL-Abfrage für die ausgewählten Produkte vorbereiten
     $sql = "SELECT id, pname, price FROM products WHERE id IN ($productIdList)";
 
+    // SQL-Abfrage ausführen
     $result = $connection->query($sql);
 
+    // Überprüfen, ob die Abfrage erfolgreich war
     if ($result) {
         $data = array();
 
+        // Results durchlaufen und Daten in das Array einfügen
         while ($row = $result->fetch_assoc()) {
             $data[] = $row;
         }
 
+        // HTTP-Header für JSON-Antwort setzen
         header('Content-Type: application/json');
 
+        // Überprüfen, ob Daten vorhanden sind und JSON-Antwort senden
         if (!empty($data)) {
             echo json_encode($data);
         } else {
-            echo json_encode(array("message" => "No products found"));
+            // Wenn keine Produkte gefunden wurden-> entsprechende JSON-Nachricht senden
+            echo json_encode(array("message" => "Keine Produkte gefunden"));
         }
     } else {
-        echo json_encode(array("message" => "Error executing query"));
+        // Bei einem Fehler in der SQL-Abfrage -> entsprechende JSON-Nachricht senden
+        echo json_encode(array("message" => "Fehler beim Ausführen der Abfrage"));
     }
 } else {
-    echo json_encode(array("message" => "No IDs provided"));
+    // Falls keine Produkt-IDs im POST-Request vorhanden sind -> entsprechende JSON-Nachricht senden
+    echo json_encode(array("message" => "Keine IDs bereitgestellt"));
 }
 
+// Verbindung zur Datenbank schließen
 $connection->close();
 ?>
